9ca372b44dcec3cb860c049c9c7d5cc36aa025c6
kim <rlaguswhd19@naver.com>
2019-10-01 09:19:41 +0900

    explorer auction

--- haribo-frontend/pages/explorer/auctionList.vue.js
+++ haribo-frontend/pages/explorer/auctionList.vue.js
@@ -24,12 +24,12 @@ var explorerAuctionView = Vue.component("ExplorerView", {
                                     <span class="badge badge-primary" v-if="items[index] && !items[index].ended">Processing</span>
                                     <span class="badge badge-danger" v-if="items[index] && items[index].ended">Ended</span>
                                 </td>
-                                <td>{{ items[index] && items[index].higestBid }} ETH</td>
+                                <td>{{ items[index] && items[index].highest_bid }} ETH</td>
                                 <td>
-                                    <span v-if="items[index] && items[index].higestBid != 0">{{ items[index] && items[index].higestBidder | truncate(15) }}</span>
-                                    <span v-if="items[index] && items[index].higestBid == 0">-</span>
+                                    <span v-if="items[index] && items[index].highest_bid != 0">{{ items[index] && items[index].highest_bidder | truncate(15) }}</span>
+                                    <span v-if="items[index] && items[index].highest_bid == 0">-</span>
                                 </td>
-                                <td>{{ items[index] && items[index].endTime.toLocaleString() }}</td>
+                                <td>{{ items[index] && items[index].end_date.toLocaleString() }}</td>
                             </tr>
                         </tbody>
                     </table>
@@ -50,5 +50,14 @@ var explorerAuctionView = Vue.component("ExplorerView", {
      * 1. AuctionFactory 컨트랙트로부터 경매컨트랙트 주소 리스트를 가져옵니다.
      * 2. 각 컨트랙트 주소로부터 경매의 상태(state) 정보를 가져옵니다.
      * */
+    var scope = this;
+    exploreService.findAllAuction(function(data){
+        for (let index = 0; index < data.length; index++) {
+            scope.contracts.push(data[index].contract_address)
+        }
+        scope.items = data;
+        console.log(scope.contracts)
+    })
+
   }
 });
